873d1cdafad83f374fb822fcaeea217609f36156
macky miro <ask.macky.miro@gmail.com>
2019-03-28 22:33:03 +0800

    added source code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -222,7 +222,7 @@
           </div>
 			-->
           <!-- DataTables Example -->
-          <div class="card mb-3">
+          <!--<div class="card mb-3">
             <div class="card-header">
               <i class="fa fa-table"></i>
               Data Table Example</div>
@@ -711,7 +711,7 @@
               </div>
             </div>
             <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-          </div>
+          </div>-->
 
         </div>
         <!-- /.container-fluid -->
